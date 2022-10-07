e39fd76ee753fe7eb3f995f8e1db107b81c89445
Lukas <Lukas@DESKTOP-34JQQMV>
2022-10-07 13:35:48 +0200

    Add project files.

---
+++ Data/LoginClass.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace MyLogin.Data
+{
+    public class LoginClass
+    {
+        private string username;
+        private string password;
+
+    public LoginClass(string username, string password)
+        {
+            this.username = username;
+            this.password = password;
+        }
+        public void SQL()
+        {
+            SqlConnection conn = new SqlConnection("Data Source=.;Initial Catalog=LoginDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            conn.Open();
+
+                SqlCommand command = new SqlCommand(
+            //    "Select LoginUser from LoginDB", conn);
+               "Select * FROM Brugere Where [Name] =@userlogin  AND [Password] =@passwordlogin", conn);
+            command.Parameters.Add("@userlogin", SqlDbType.NVarChar).Value = username;
+            command.Parameters.Add("@passwordlogin", SqlDbType.NVarChar).Value = password;
+            //// int result = command.ExecuteNonQuery();
+            //using (SqlDataReader reader = command.ExecuteReader())
+            //{
+            // while (reader.Read())
+            //   {
+            //       Console.WriteLine(String.Format("{0}", reader["LoginUser"]));
+            //   }
+            //}
+
+            SqlDataAdapter sda = new SqlDataAdapter(command);
+            DataTable dataTable = new DataTable();
+            sda.Fill(dataTable);
+
+            if (dataTable.Rows.Count <= 0)
+            {
+                Console.WriteLine("Username or Password does not exist");
+            }
+
+            else
+            {
+                Console.WriteLine($"Successfully logged in with {username}");
+            }
+
+            conn.Close();
+        }
+
+    }
+}
---
+++ Data/Signup.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace MyLogin.Data
+{
+    public class CreateClass
+    {
+        private string username;
+        private string password;
+        private string email;
+
+    public CreateClass(string username, string password, string email)
+    {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public void CreateSQL()
+        {
+            SqlConnection conn = new SqlConnection("Data Source=.;Initial Catalog=LoginDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            conn.Open();
+
+            SqlCommand command = new SqlCommand(
+            "INSERT INTO brugere ([Name] , Email,  [Password]) Values (@CreateUser,@CreateEmail,@CreatePassword)", conn);
+            command.Parameters.Add("@CreateUser", SqlDbType.NVarChar).Value = username;
+            command.Parameters.Add("@CreateEmail", SqlDbType.NVarChar).Value = email;
+            command.Parameters.Add("@CreatePassword", SqlDbType.NVarChar).Value = password;
+            command.ExecuteNonQuery();
+        }
+    }
+}
